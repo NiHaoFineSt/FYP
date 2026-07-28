74be803a9847561894790573590e62a72db30a00
NiHauFineSt <syafiqahfarhana718@gmail.com>
2026-07-28 23:45:25 +0800

    Fix root path inclusion and router working directory

--- api/index.php
+++ api/index.php
@@ -1,9 +1,10 @@
 <?php
-// Capture requested path
+// Set working directory to project root
+chdir(__DIR__ . '/..');
+
 $uri = $_SERVER['REQUEST_URI'];
 $path = parse_url($uri, PHP_URL_PATH);
 
-// Default home page routing (e.g. login.php or homepage.html)
 if ($path === '/' || $path === '') {
     if (file_exists(__DIR__ . '/../login.php')) {
         require __DIR__ . '/../login.php';
@@ -13,7 +14,6 @@ if ($path === '/' || $path === '') {
     exit;
 }
 
-// Locate requested file relative to root directory
 $file = __DIR__ . '/..' . $path;
 
 if (file_exists($file) && is_file($file)) {
@@ -21,7 +21,6 @@ if (file_exists($file) && is_file($file)) {
     if ($ext === 'php') {
         require $file;
     } else {
-        // Serve static asset (CSS, JS, Images)
         return false;
     }
 } else {

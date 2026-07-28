822358b4ae74303369f6fee864a8272fa7bbad9d
NiHauFineSt <syafiqahfarhana718@gmail.com>
2026-07-29 03:14:03 +0800

    testing

--- recycling_staff/managerequest.php
+++ recycling_staff/managerequest.php
@@ -36,7 +36,7 @@ $waiting_count = $count_res->fetch_assoc()['total'] ?? 0;
                 <a href="staffscan.php">Scan QR</a>
                 <a href="factory.php">Factory</a>
                 <a href="staffchat.php">Staff Chat</a>
-                <a href="staffprofile.php">Profile</a>
+                <a href="../staffprofile.php">Profile</a>
                 <div class="nav-divider"></div>
                 <a href="../logout.php" class="logout">Logout</a>
             </nav>
